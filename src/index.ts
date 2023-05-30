b18222f3d8903a63326f14de9187b7a5dd21e943
Marius Linders <marius.linders@orange-juice.nl>
2023-05-30 08:31:54 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,146 +1,72 @@
 import { EventAggregator } from "oj-eventaggregator"
+import { singleton } from "oj-promise-utils"
+import { memoize } from "oj-utils"
 
-export const googleMapsLoader = (key: string) =>
-  new Promise<any>(res => {
+export interface IGeoCodeLocation { lat: number, lng: number }
+
+export interface IGeoCodeLocationSearch { location: IGeoCodeLocation }
+
+export interface IGeoCodeAddressSearch { componentRestrictions?: { postalCode: string }, address: string, region: string }
+
+export class Geocoder extends EventAggregator<{
+  load: Geocoder
+}> {
+  private google = singleton(() => new Promise<any>(res => {
     const cbFnHash = `gml_${Math.random().toString(36).substring(7)}`
     window[cbFnHash] = () => res(window["google"])
     const script = document.createElement("script")
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=${cbFnHash}`
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${this.key}&callback=${cbFnHash}`
     document.body.appendChild(script)
+    this.emit("load", this)
+  }))
+  private readonly key: string
+
+  constructor(options: { key: string }) {
+    super()
+    this.key = options.key
+  }
+
+  geocode = memoize(async (search: IGeoCodeLocationSearch | IGeoCodeAddressSearch) => {
+    const google = await this.google()
+    const geocoder = new google.maps.Geocoder()
+
+    return new Promise<any[]>((res, rej) => geocoder.geocode(search, (results, status) => {
+      if (status == google.maps.GeocoderStatus.OK)
+        res(results)
+      else
+        rej(status)
+    }))
   })
 
-export interface IGeoCodeLocationSearch { location: ILocation }
-
-export interface IGeoCodeAddressSearch { address: string, region: string }
-
-export type IResults = IResult[]
-
-export interface IResult {
-  address_components: IAddresscomponent[]
-  formatted_address: string
-  geometry: IGeometry
-  place_id: string
-  plus_code?: {
-    compound_code: string
-    global_code: string
-  }
-  types: string[]
-}
-
-export interface IGeometry {
-  location: ILocation
-  location_type: string
-  viewport: IViewport
-  bounds?: IViewport
-}
-
-export interface IViewport {
-  south: number
-  west: number
-  north: number
-  east: number
-}
-
-export interface ILocation {
-  lat: number
-  lng: number
-}
-
-export interface IAddresscomponent {
-  long_name: string
-  short_name: string
-  types: string[]
-}
-
-export class Geocoder extends EventAggregator<"load"> {
-  google: any
-
-  constructor(key: string) {
-    super()
-    googleMapsLoader(key)
-      .then(google => {
-        this.google = google
-        this.emit("load", this)
-      })
-  }
-
-  // load() {
-  //   if (this.google !== undefined) return Promise.resolve(this.google)
-  //   return new Promise<any>(res => this.once("load", () => res(this.google)))
-  // }
-
-  async find(search: IGeoCodeAddressSearch) {
-    const results = await this.geocodeLatLng(await this.findLatLng(search))
-    const cache = {}
-    return {
-      results,
-      getCountry: () => {
-        if (!cache["getCountry"])
-          cache["getCountry"] = this.firstResult(results, "country")
-        return cache["getCountry"]
-      },
-      getProvince: () => {
-        if (!cache["getProvince"])
-          cache["getProvince"] = this.firstResult(results, "administrative_area_level_1")
-        return cache["getProvince"]
-      },
-      getCity: () => {
-        if (!cache["getCity"])
-          cache["getCity"] = this.firstResult(results, "locality")
-        return cache["getCity"]
-      },
-      getCitySub: () => {
-        if (!cache["getCitySub"])
-          cache["getCitySub"] = this.firstResult(results, "sublocality")
-        return cache["getCitySub"]
-      },
-      getPostalCode: () => {
-        if (!cache["getPostalCode"])
-          cache["getPostalCode"] = this.firstResult(results, "postal_code")
-        return cache["getPostalCode"]
-      },
-      getStreet: () => {
-        if (!cache["getStreet"])
-          cache["getStreet"] = this.firstResult(results, "route")
-        return cache["getStreet"]
-      },
-      getStreetNumber: () => {
-        if (!cache["getStreetNumber"])
-          cache["getStreetNumber"] = this.firstResult(results, "street_number")
-        return cache["getStreetNumber"]
-      },
-    }
-  }
-
-  firstResult(results: IResults, type: string) {
-    for (const { address_components: a } of results) {
-      const r = a.find(x => x.types.includes(type))
-      if (r)
-        return r.long_name
-    }
-    return null
-  }
-
-  async geocode(search: IGeoCodeLocationSearch | IGeoCodeAddressSearch) {
-    return new Promise<any>((res, rej) => {
-      const geocoder = new this.google.maps.Geocoder()
-      geocoder.geocode(search, (results, status) => {
-        if (status == this.google.maps.GeocoderStatus.OK)
-          res(results)
-        else
-          rej(status)
-      })
-    })
-  }
-
-  async findLatLng(search: IGeoCodeAddressSearch): Promise<ILocation> {
+  async findLatLng(search: IGeoCodeAddressSearch) {
     const results = await this.geocode(search)
     const lat = results[0].geometry.location.lat()
     const lng = results[0].geometry.location.lng()
     return { lat, lng }
   }
 
-  async geocodeLatLng(ltLng: ILocation): Promise<IResults> {
-    return await this.geocode({ location: ltLng })
+  async geocodeLatLn(location: IGeoCodeLocation) {
+    const results = await this.geocode({ location })
+    return results
+  }
+
+  getStreet(result: any) {
+    return result.address_components.find(x => x.types.indexOf("route") !== -1).long_name as string
+  }
+
+  getPostalCode(result: any) {
+    return result.address_components.find(x => x.types.indexOf("postal_code") !== -1).long_name as string
+  }
+
+  getCity(result: any) {
+    return result.address_components.find(x => x.types.indexOf("locality") !== -1).long_name as string
+  }
+
+  getProvince(result: any) {
+    return result.address_components.find(x => x.types.indexOf("administrative_area_level_1") !== -1).long_name as string
+  }
+
+  getCountry(result: any) {
+    return result.address_components.find(x => x.types.indexOf("country") !== -1).long_name as string
   }
 }
